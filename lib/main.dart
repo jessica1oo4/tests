8ced933fa75c338660b3a0b5e5b591d72d65f9d6
Jess <jessica1oo4@gmail.com>
2020-05-25 17:45:17 -0700

    this is the color biatch!!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
